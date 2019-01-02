a6902d237f9ec3184740ea8d2d03caab60503d2f
toddhyatt <todd.hyatt@live.com>
2019-01-02 00:41:47 -0600

    initial checkin

---
+++ LinqConsole.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+namespace LinqExample
+{
+    public class LinqConsole
+    {
+        public LinqConsole()
+        {
+        }
+        public string RunTop(List<string> stringList)
+        {
+            string result = string.Empty;
+            System.Text.StringBuilder sb = new System.Text.StringBuilder();
+            foreach (string s in stringList)
+            {
+                sb.AppendLine(s);
+            }
+            result = sb.ToString();
+            var x = from y in stringList
+                    where y.Contains('-')
+                    orderby y.Split('-')[0] ,  SortX(y.Split('-')[1]) descending  
+                    group y by (y.Split('-')[0]) into z
+                    select  z.First();
+
+
+
+            return result;
+        }
+        public int SortX(string invalue)
+        {
+            int result = 0;
+            switch (invalue)
+            {
+                case "one":
+                    result = 1;
+                    break;
+                case "two":
+                    result = 2;
+                    break;
+                case "three":
+                    result = 3;
+                    break;
+                default:
+                    result = 99;
+                    break;
+
+            }
+            return result;
+
+        }
+    }
+
+
+
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+namespace LinqExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            LinqConsole lConsole=new LinqConsole();
+            string [] listOfValues = new [] {"1-one","1-two","2-three"};
+            Console.WriteLine(lConsole.RunTop(new List<string>(listOfValues)));
+
+        }
+    }
+}
